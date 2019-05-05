1c6c2426c63aba3ef32e3facbd0feb2a145b522a
정승욱 <pdaniel0124@gmail.com>
2019-05-05 19:40:58 +0900

    top pager 구현

--- Source/UI/Home/ViewController/HomeViewController.swift
+++ Source/UI/Home/ViewController/HomeViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet private weak var scrollView: UIScrollView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
