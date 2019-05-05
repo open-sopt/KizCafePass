a88d0cb30866cbb8193610a5b4a9d999d840f320
정승욱 <pdaniel0124@gmail.com>
2019-05-05 20:58:05 +0900

    추천/ 인기 카드 뷰 구성

---
+++ Source/UI/Home/View/HomeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeRecommendCollectionViewCell.swift
+//  KizCafePass
+//
+//  Created by seungwook-jung on 05/05/2019.
+//  Copyright © 2019 정승욱. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Source/UI/Home/ViewController/HomeViewController.swift
+++ Source/UI/Home/ViewController/HomeViewController.swift
@@ -9,15 +9,76 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     @IBOutlet private weak var scrollView: UIScrollView?
+    @IBOutlet private weak var recommendCollectionView: UICollectionView?
+    @IBOutlet private weak var popularCollectionView: UICollectionView?
+    
+    private let homeCollectionViewCellID = "HomeCollectionViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initRecommendCollcetionView()
+        self.initPopularCollcetionView()
         
     }
-
-
+    
+    private func initRecommendCollcetionView() {
+        self.recommendCollectionView?.register(UINib(nibName: "HomeCollectionViewCell",
+                                                     bundle: nil),
+                                               forCellWithReuseIdentifier: self.homeCollectionViewCellID)
+        self.recommendCollectionView?.dataSource = self
+        self.recommendCollectionView?.delegate = self
+    }
+    
+    private func initPopularCollcetionView() {
+        self.popularCollectionView?.register(UINib(nibName: "HomeCollectionViewCell",
+                                                     bundle: nil),
+                                               forCellWithReuseIdentifier: self.homeCollectionViewCellID)
+        self.popularCollectionView?.dataSource = self
+        self.popularCollectionView?.delegate = self
+    }
 }
 
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 130, height: 140)
+    }
+}
+
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // recommend
+        if collectionView.restorationIdentifier == "RecommendCollectionView" {
+            return 10
+        }
+        
+        // popular
+        if collectionView.restorationIdentifier == "PopularCollectionView" {
+            return 6
+        }
+        
+        return 0
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let recommendCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.homeCollectionViewCellID,
+                                                               for: indexPath)
+        
+        guard let contentCell = recommendCell as? HomeCollectionViewCell else {
+            return recommendCell
+        }
+        
+        return contentCell
+        
+    }
+    
+}
