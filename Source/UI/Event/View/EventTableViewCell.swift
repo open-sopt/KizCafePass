34a3a1e7a893f30bcde05083fd3ac40cd5cc88c7
yejin <dpwls7753@gmail.com>
2019-05-05 17:21:38 +0900

    EventViewController 생성

---
+++ Source/UI/Event/View/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  KizCafePass
+//
+//  Created by yejin on 05/05/2019.
+//  Copyright © 2019 정승욱. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Source/UI/Event/ViewController/EventViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EventViewController.swift
+//  KizCafePass
+//
+//  Created by yejin on 05/05/2019.
+//  Copyright © 2019 정승욱. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
